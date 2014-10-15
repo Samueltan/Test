aa95f2776dab5c459e7c364d4ea4eb7e22e9ae16
Samuel Tan <samueltango@gmail.com>
2014-10-15 17:46:55 -0400

    folder structure update

---
+++ spring/GradleTest/src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package main.java.hello;
+
+/**
+ * Created by Samuel on 2014/10/15.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ spring/GradleTest/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.java.hello;
+
+/**
+ * Created by Samuel on 2014/10/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
