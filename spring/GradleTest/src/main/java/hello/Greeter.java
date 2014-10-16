ff61532f8cf09dba31f360e7abdb160328f9f9ca
Samuel Tan <samueltango@gmail.com>
2014-10-16 11:19:16 -0400

    update the hello function

--- spring/GradleTest/src/main/java/hello/Greeter.java
+++ spring/GradleTest/src/main/java/hello/Greeter.java
@@ -4,7 +4,7 @@ package hello;
  * Created by Samuel on 2014/10/15.
  */
 public class Greeter {
-    public String sayHello() {
-        return "Hello world!";
+    public String sayHello(String s) {
+        return "Hello world, "  + s;
     }
 }
